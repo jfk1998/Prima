4625c7c6b680dc8d8bd36b70373f9099740e0207
jonas kern <dreptschar@gmail.com>
2019-10-11 13:48:16 +0200

    L2

---
+++ L02_FirstFudge/Main.js
@@ -0,0 +1,12 @@
+"use strict";
+var L02_FirstFudge;
+(function (L02_FirstFudge) {
+    var fudge = FudgeCore;
+    window.addEventListener("load", handleLoad);
+    function handleLoad() {
+        let canvas = document.querySelector("canvas");
+        console.log(canvas);
+        let Viewport = new fudge.Viewport();
+    }
+})(L02_FirstFudge || (L02_FirstFudge = {}));
+//# sourceMappingURL=Main.js.map
---
+++ L02_FirstFudge/Main.ts
@@ -0,0 +1,16 @@
+namespace L02_FirstFudge {
+import fudge = FudgeCore;
+window.addEventListener("load", handleLoad);
+
+function handleLoad(): void {
+    let canvas: HTMLCanvasElement = document.querySelector("canvas");
+    console.log(canvas);
+
+    let Viewport: fudge.Viewport = new fudge.Viewport();
+
+
+
+}
+   
+
+}
