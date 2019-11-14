8e885ff954e826c4a3b00dacb8f668036a1f0b81
jonas kern <dreptschar@gmail.com>
2019-11-14 14:03:21 +0100

    added del oro fudge core

--- L04_3DTetris/Main.js
+++ L04_3DTetris/Main.js
@@ -1,41 +1,64 @@
 "use strict";
-var L04_3DTetris;
-(function (L04_3DTetris) {
-    var fudge = FudgeCore;
-    window.addEventListener("load", handleLoad);
+var L07_FudgeCraft_Fragments;
+(function (L07_FudgeCraft_Fragments) {
+    var ƒ = FudgeCore;
+    window.addEventListener("load", hndLoad);
     let viewport;
-    function handleLoad() {
-        let canvas = document.querySelector("canvas");
-        fudge.RenderManager.initialize();
-        let cam = new fudge.ComponentCamera();
-        cam.pivot.translateZ(20);
-        viewport = new fudge.Viewport();
-        viewport.initialize("Viewport", sceneNode, cam, canvas);
-        fudge.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, updateLoopFrame);
-        fudge.Loop.start();
+    let game;
+    let rotate = ƒ.Vector3.ZERO();
+    function hndLoad(_event) {
+        const canvas = document.querySelector("canvas");
+        ƒ.RenderManager.initialize(true);
+        ƒ.Debug.log("Canvas", canvas);
+        let cmpCamera = new ƒ.ComponentCamera();
+        cmpCamera.pivot.translate(new ƒ.Vector3(2, 3, 10));
+        cmpCamera.pivot.lookAt(ƒ.Vector3.ZERO());
+        game = new ƒ.Node("FudgeCraft");
+        // let cube: Cube = new Cube(CUBE_TYPE.BLUE);
+        let fragment = new L07_FudgeCraft_Fragments.Fragment(0);
+        // ƒ.Debug.log("Fragment", fragment);
+        fragment.addComponent(new ƒ.ComponentTransform());
+        game.appendChild(fragment);
+        fragment = new L07_FudgeCraft_Fragments.Fragment(1);
+        // ƒ.Debug.log("Fragment", fragment);
+        fragment.addComponent(new ƒ.ComponentTransform(ƒ.Matrix4x4.TRANSLATION(ƒ.Vector3.X(3))));
+        game.appendChild(fragment);
+        fragment = new L07_FudgeCraft_Fragments.Fragment(2);
+        // ƒ.Debug.log("Fragment", fragment);
+        fragment.addComponent(new ƒ.ComponentTransform(ƒ.Matrix4x4.TRANSLATION(ƒ.Vector3.X(-3))));
+        game.appendChild(fragment);
+        let cmpLight = new ƒ.ComponentLight(new ƒ.LightDirectional(ƒ.Color.WHITE));
+        cmpLight.pivot.lookAt(new ƒ.Vector3(0.5, 1, 0.8));
+        game.addComponent(cmpLight);
+        viewport = new ƒ.Viewport();
+        viewport.initialize("Viewport", game, cmpCamera, canvas);
+        ƒ.Debug.log("Viewport", viewport);
         viewport.draw();
+        ƒ.Debug.log("Game", game);
+        window.addEventListener("keydown", hndKeyDown);
     }
-    function updateLoopFrame(_event) {
-        fudge.RenderManager.update();
-        viewport.draw();
-    }
-    function detectHit(hitterPosition, collisionObject) {
-        let objectScaling = collisionObject.getComponent(fudge.ComponentMesh).pivot.scaling;
-        let objectPosition = collisionObject.cmpTransform.local.translation;
-        let bottomRight = new fudge.Vector3(objectPosition.x + (objectScaling.x / 2), objectPosition.y + (objectScaling.y / 2), 0);
-        let topLeft = new fudge.Vector3(objectPosition.x - (objectScaling.x / 2), objectPosition.y - (objectScaling.y / 2));
-        //hitting from Right or Left
-        if (hitterPosition.x > topLeft.x && hitterPosition.x < bottomRight.x) {
-            //hitting from top or Bottom
-            if (hitterPosition.y > topLeft.y && hitterPosition.y < bottomRight.y) {
-                return true;
-            }
+    function hndKeyDown(_event) {
+        //let rotate: ƒ.Vector3 = ƒ.Vector3.ZERO();
+        switch (_event.code) {
+            case ƒ.KEYBOARD_CODE.ARROW_UP:
+                rotate.add(ƒ.Vector3.X(-1));
+                break;
+            case ƒ.KEYBOARD_CODE.ARROW_DOWN:
+                rotate.add(ƒ.Vector3.X(1));
+                break;
+            case ƒ.KEYBOARD_CODE.ARROW_LEFT:
+                rotate.add(ƒ.Vector3.Y(-1));
+                break;
+            case ƒ.KEYBOARD_CODE.ARROW_RIGHT:
+                rotate.add(ƒ.Vector3.Y(1));
+                break;
         }
-        else
-            return false;
+        for (let fragment of game.getChildren()) {
+            // fragment.cmpTransform.local.rotate(rotate);
+            fragment.cmpTransform.local.rotation = rotate;
+        }
+        ƒ.RenderManager.update();
+        viewport.draw();
     }
-    function createScene() {
-        return sceneNode;
-    }
-})(L04_3DTetris || (L04_3DTetris = {}));
+})(L07_FudgeCraft_Fragments || (L07_FudgeCraft_Fragments = {}));
 //# sourceMappingURL=Main.js.map
--- L04_3DTetris/Main.ts
+++ L04_3DTetris/Main.ts
@@ -1,72 +1,76 @@
+namespace L07_FudgeCraft_Fragments {
+    import ƒ = FudgeCore;
 
-namespace L04_3DTetris {
-    import fudge = FudgeCore;
-    window.addEventListener("load", handleLoad);
+    window.addEventListener("load", hndLoad);
+    let viewport: ƒ.Viewport;
+    let game: ƒ.Node;
+    let rotate: ƒ.Vector3 = ƒ.Vector3.ZERO();
 
-    let viewport: fudge.Viewport;
+    function hndLoad(_event: Event): void {
+        const canvas: HTMLCanvasElement = document.querySelector("canvas");
+        ƒ.RenderManager.initialize(true);
+        ƒ.Debug.log("Canvas", canvas);
+
+        let cmpCamera: ƒ.ComponentCamera = new ƒ.ComponentCamera();
+        cmpCamera.pivot.translate(new ƒ.Vector3(2, 3, 10));
+        cmpCamera.pivot.lookAt(ƒ.Vector3.ZERO());
+
+        game = new ƒ.Node("FudgeCraft");
+
+        // let cube: Cube = new Cube(CUBE_TYPE.BLUE);
+        let fragment: Fragment = new Fragment(0);
+        // ƒ.Debug.log("Fragment", fragment);
+        fragment.addComponent(new ƒ.ComponentTransform());
+        game.appendChild(fragment);
+
+        fragment = new Fragment(1);
+        // ƒ.Debug.log("Fragment", fragment);
+        fragment.addComponent(new ƒ.ComponentTransform(ƒ.Matrix4x4.TRANSLATION(ƒ.Vector3.X(3))));
+        game.appendChild(fragment);
+
+        fragment = new Fragment(2);
+        // ƒ.Debug.log("Fragment", fragment);
+        fragment.addComponent(new ƒ.ComponentTransform(ƒ.Matrix4x4.TRANSLATION(ƒ.Vector3.X(-3))));
+        game.appendChild(fragment);
+
+        let cmpLight: ƒ.ComponentLight = new ƒ.ComponentLight(new ƒ.LightDirectional(ƒ.Color.WHITE));
+        cmpLight.pivot.lookAt(new ƒ.Vector3(0.5, 1, 0.8));
+        game.addComponent(cmpLight);
 
 
-
-    function handleLoad(): void {
-        let canvas: HTMLCanvasElement = document.querySelector("canvas");
-        
-        fudge.RenderManager.initialize();
-
-        let cam: fudge.ComponentCamera = new fudge.ComponentCamera();
-        cam.pivot.translateZ(20);
-
-        viewport = new fudge.Viewport();
-
-        viewport.initialize("Viewport", sceneNode, cam, canvas);
-
-        fudge.Loop.addEventListener(fudge.EVENT.LOOP_FRAME, updateLoopFrame);
-        fudge.Loop.start();
+        viewport = new ƒ.Viewport();
+        viewport.initialize("Viewport", game, cmpCamera, canvas);
+        ƒ.Debug.log("Viewport", viewport);
 
         viewport.draw();
 
+        ƒ.Debug.log("Game", game);
+
+        window.addEventListener("keydown", hndKeyDown);
     }
 
-    function updateLoopFrame(_event: Event): void {
+    function hndKeyDown(_event: KeyboardEvent): void {
+        //let rotate: ƒ.Vector3 = ƒ.Vector3.ZERO();
+        switch (_event.code) {
+            case ƒ.KEYBOARD_CODE.ARROW_UP:
+                rotate.add(ƒ.Vector3.X(-1));
+                break;
+            case ƒ.KEYBOARD_CODE.ARROW_DOWN:
+                rotate.add(ƒ.Vector3.X(1));
+                break;
+            case ƒ.KEYBOARD_CODE.ARROW_LEFT:
+                rotate.add(ƒ.Vector3.Y(-1));
+                break;
+            case ƒ.KEYBOARD_CODE.ARROW_RIGHT:
+                rotate.add(ƒ.Vector3.Y(1));
+                break;
+        }
+        for (let fragment of game.getChildren()) {
+            // fragment.cmpTransform.local.rotate(rotate);
+            fragment.cmpTransform.local.rotation = rotate;
+        }
 
- 
-        fudge.RenderManager.update();
+        ƒ.RenderManager.update();
         viewport.draw();
-
     }
-
-    function detectHit(hitterPosition: fudge.Vector3, collisionObject: fudge.Node): boolean{
-        
-        let objectScaling: fudge.Vector3 = (collisionObject.getComponent(fudge.ComponentMesh) as fudge.ComponentMesh).pivot.scaling;
-        let objectPosition: fudge.Vector3 = collisionObject.cmpTransform.local.translation;
-
-        let bottomRight: fudge.Vector3 = new fudge.Vector3(objectPosition.x + (objectScaling.x/2), objectPosition.y + (objectScaling.y/2),0 );
-        let topLeft: fudge.Vector3 = new fudge.Vector3(objectPosition.x - (objectScaling.x/2), objectPosition.y - (objectScaling.y/2));
-
-        //hitting from Right or Left
-        if(hitterPosition.x > topLeft.x && hitterPosition.x < bottomRight.x)
-        {
-             //hitting from top or Bottom
-            if(hitterPosition.y > topLeft.y && hitterPosition.y < bottomRight.y )
-            {
-
-                return true;
-
-            }
-
-        }else return false;
-        
-    }
-
-
-
-
-    function createScene(): fudge.Node {
-      
-
-        return sceneNode;
-    }
-
-
-
-
 }
